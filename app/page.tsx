4a52c6261da7c88c51bd92c22017720eb7cd27e8
Oscar Leal <oscarleal234@gmail.com>
2024-04-08 12:42:41 -0500

    fixed home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,26 @@
-'use client'
-import {Image} from "@nextui-org/react";
-import TypingEffect from "./componenets/typing";
+'use client';
+import { Image } from '@nextui-org/react';
+import TypingEffect from './componenets/typing';
 
-
-import { useState,useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [welome, setWelcome] = useState('Welcome');
 
-  const [welome,setWelcome]=useState('Welcome');
-  
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen">
       <div className="flex flex-row justify-around items-center flex-wrap m-5 p-5 ">
-        <div><h1 className="text-6xl text-white font-bold text-center">O's <TypingEffect/></h1></div>
-      
-      <div className="rounded-full m-5">
-      <Image
-      isZoomed
-      width={300}
-      alt="Me"
-      src="/Me.JPG"
-      
-      
-    />
+        <div>
+          <h1 className="text-6xl text-white font-bold text-center">
+            O's <TypingEffect />
+          </h1>
+        </div>
+
+        <div className="rounded-full m-5">
+          <Image isZoomed width={300} alt="Me" src="/Me.JPG" />
+        </div>
       </div>
-    
-      </div>
-      
+
       <video
         src={require('../public/BG.mp4')}
         autoPlay
