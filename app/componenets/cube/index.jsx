0b8dc79fa61a792bf8d16dd9909fb221c7b04b3a
Oscar Leal <oscarleal234@gmail.com>
2024-04-16 15:15:50 -0500

    added a 3d model

--- app/componenets/cube/index.jsx
+++ app/componenets/cube/index.jsx
@@ -1,33 +1,31 @@
 'use client';
 import React, { useRef } from 'react';
 import styles from './style.module.scss';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { ambientLight } from 'three';
+
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 export default function index() {
   return (
     <div className={styles.main}>
       <Canvas>
         <ambientLight intensity={2} />
         <directionalLight position={[2, 1, 1]} />
-        <Cube />
+        <Model />
       </Canvas>
     </div>
   );
 }
 
-function Cube() {
+function Model() {
+  const gltf = useLoader(GLTFLoader, '../astro.glb');
   const mesh = useRef(null);
 
   useFrame((state, delta) => {
     mesh.current.rotation.x += delta * 0.5;
     mesh.current.rotation.y += delta * 0.5;
-    mesh.current.rotation.z += delta * 0.5;
+    // mesh.current.rotation.z += delta * 0.5;
   });
 
-  return (
-    <mesh ref={mesh}>
-      <boxGeometry args={[2.5, 2.5, 2.5]} />
-      <meshStandardMaterial color={'green'} />
-    </mesh>
-  );
+  return <primitive object={gltf.scene} ref={mesh} />;
 }
