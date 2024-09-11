3ed053bb421e40019dc2a39570bbe1e1623736f0
Oscar Leal <oscarleal234@gmail.com>
2024-09-10 22:29:50 -0500

    final merge for blog system

--- app/componenets/aboutpage/portfoliobutton.tsx
+++ app/componenets/aboutpage/portfoliobutton.tsx
@@ -1,19 +1,23 @@
 import { Button } from '@nextui-org/react';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 
 const PortfolioButton = () => {
   return (
-    <div>      <div className="flex justify-center m-5 mt-10">
-    <Button
-      color="success"
-      variant="bordered"
-      className="hover:dark:text-green-500 hover:text-green-500 border-green-500 dark:text-white text-black"
-    >
-      <Link href={'/portfolio'}>Portfolio</Link>
-    </Button>
-  </div></div>
-  )
-}
+    <div>
+      {' '}
+      <div className="flex justify-center m-5 mt-10">
+        <Button
+          color="success"
+          variant="bordered"
+          className="hover:dark:text-green-500 hover:text-green-500 border-green-500 dark:text-white text-black"
+        >
+          <Link href={'/portfolio'}>Portfolio</Link>
+        </Button>
+      </div>
+    </div>
+    //test
+  );
+};
 
-export default PortfolioButton
+export default PortfolioButton;
