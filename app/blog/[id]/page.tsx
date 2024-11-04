0eb4bb660228d2246b498cefb97e35dbb49af506
Oscar Leal <oscarleal234@gmail.com>
2024-11-04 14:46:18 -0600

    edited path

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -127,13 +127,13 @@ const Blog = () => {
               Back to Blogs
             </Button>
           </Link>
-          <Link href={'/portfolio'}>
+          <Link href={'/projects'}>
             <Button
               color="success"
               variant="bordered"
               className="hover:dark:text-green-500 hover:text-green-500 border-green-500 dark:text-white text-black"
             >
-              Portfolio
+              Projects
             </Button>
           </Link>
         </div>
