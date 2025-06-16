81347b7af30c13bafe023cba406d14131e93d6cf
Oscarl214 <oscarleal234@gmail.com>
2025-06-15 20:39:48 -0500

    removed files

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import Cube from '../componenets/2024archive/cube';
-import { ContactMotion } from '../componenets/2024archive/contactmotion';
-import { EarthMotion } from '../componenets/2024archive/earthmotion';
-import ContactForm from '../componenets/2024archive/contactform';
 import CalendlyForm from '../componenets/contactpage/calendly';
 const Contact = () => {
   return (
